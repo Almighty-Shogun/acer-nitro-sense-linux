fd8e67c938994a6db6b7cded194f21ec9de7f4ca
Shogun <alm1ghtyshogun1998@gmail.com>
2026-08-23 14:51:12 +0200

    fix: clean up extracted installer directory after install

--- packaging/run/self-extract-header.sh
+++ packaging/run/self-extract-header.sh
@@ -7,6 +7,6 @@ trap cleanup EXIT INT TERM
 
 sed '1,/^__ANS_ARCHIVE_BELOW__$/d' "$0" | tar -xz -C "$tmp"
 dir=$(find "$tmp" -mindepth 1 -maxdepth 1 -type d | head -1)
-exec "$dir/install.sh" "$@"
+"$dir/install.sh" "$@"
 
 __ANS_ARCHIVE_BELOW__
